830a1eaea3b956ed132a8b61eba10153522ce00b
platonlukyanov <76910837+platonlukyanov@users.noreply.github.com>
2021-12-30 16:19:13 +0300

    Changed get_ml_prediction return type

--- get_ml_prediction.py
+++ get_ml_prediction.py
@@ -4,5 +4,5 @@ clf = joblib.load('pickles/model.pkl')
 vectoriser = joblib.load('pickles/vectoriser.pkl')
 
 
-def get_ml_prediction(text: str) -> str:
+def get_ml_prediction(text: str) -> 1 | 0:
     return clf.predict(vectoriser.transform([text]))[0]
